9f054dd7648820f0a35d181bf6924523218802c1
RyoTsuchida <138351310+ryo00415@users.noreply.github.com>
2023-09-17 07:41:18 +0000

    photo

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,16 @@
 class HomesController < ApplicationController
   def top
+    # タイムライン上の投稿を取得するクエリを追加する
+    # 例: 最新の投稿を取得する場合、投稿モデルから最新の投稿を取得するコードを記述
+    @timeline_photos = Photo.order(created_at: :desc).limit(10)
+
+    # 他の必要なロジックや変数の設定を行う
+
+    # ビューに渡すデータを設定する
+    # この行は必要に応じて変更してください
+    @some_other_variable = "Some data"
+
+    # ビューを描画する
+    render "top"
   end
 end
--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,11 +1,11 @@
 class TimelinesController < ApplicationController
   def index
     # タイムラインに表示するコンテンツを取得するロジックを記述
-    @timeline_posts = Post.all.order(created_at: :desc)  # 例: 投稿を新しい順に取得
+    @timeline_photos = Photo.all.order(created_at: :desc)  # 例: 投稿を新しい順に取得
 
     respond_to do |format|
       format.html
-      format.json { render json: @timeline_posts }  # タイムラインデータをJSON形式で提供する場合
+      format.json { render json: @timeline_photos }  # タイムラインデータをJSON形式で提供する場合
     end
   end
 end
